72ccd4c85bf0e6ca8db137692533fd3729cf4b3f
Humberto <humberto@2com.com.mx>
2018-07-19 14:36:18 -0500

    descarga de acta

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -12,6 +12,7 @@ use app\models\ContactForm;
 use app\components\AccessControlExtend;
 use app\models\WrkActasRetencion;
 use yii\web\HttpException;
+use kartik\mpdf\Pdf;
 
 class SiteController extends Controller
 {
@@ -46,6 +47,85 @@ class SiteController extends Controller
             'model' => $acta,
         ]);
 
+    }
+
+    public function actionDownload(){
+
+        // get your HTML raw content without any layouts or scripts
+    $content = $this->renderPartial('_acta');
+    
+    // setup kartik\mpdf\Pdf component
+    $pdf = new Pdf([
+        // set to use core fonts only
+        'mode' => Pdf::MODE_CORE, 
+        // A4 paper format
+        'format' => Pdf::FORMAT_A4, 
+        // portrait orientation
+        'orientation' => Pdf::ORIENT_PORTRAIT, 
+        // stream to browser inline
+        'destination' => Pdf::DEST_BROWSER, 
+        // your html content input
+        'content' => $content,  
+        'options'=>[
+            "setAutoTopMargin"=>true,
+            //"setAutoBottomMargin"=>true
+        ],
+        // format content from your own css file if needed or use the
+        // enhanced bootstrap css built by Krajee for mPDF formatting 
+       // 'cssFile' => '@vendor/kartik-v/yii2-mpdf/assets/kv-mpdf-bootstrap.min.css',
+        // any css to be embedded if required
+        'cssInline' => ' body{
+            font-family: Arial, "Helvetica";
+            font-size: 14px;
+        }
+        table{
+            border-spacing: 0;
+            width: 100%;
+        }', 
+         // set mPDF properties on the fly
+        //'options' => ['title' => 'Krajee Report Title'],
+         // call mPDF methods on the fly
+         
+        'methods' => [ 
+          'SetHeader'=>['<table cellpadding="0" cellspacing="0" style="border-left: 1px solid black; border-top: 1px solid black; border-right: 1px solid black; border-bottom: none; border-collapse: collapse; width: 100%;">
+          <tr>
+              <td align="left" style="padding: 8px 12px;">
+                  <img src="https://dev.2geeksonemonkey.com/senasica/admin/web/webAssets/images/logo-sagarpa-horizontal.png" width="224" alt="Sagarpa">
+              </td>
+              <td align="right" style="padding: 8px 12px;">
+                  <img src="https://dev.2geeksonemonkey.com/senasica/admin/web/webAssets/images/logo-senasica-horizontal.png" width="160" alt="Senasica">
+              </td>
+          </tr>
+
+          <tr>
+              <td align="center" colspan="2" style="padding: 24px 0;">
+                  <h1 style="font-size: 18px; font-style: italic; font-weight: bold; margin-bottom: 4px; margin-top: 0; text-transform: uppercase;">Secretaría de Agricultura, Ganadería, Desarrollo Rural, Pesca y Alimentación</h1>
+                  <h2 style="font-size: 18px; font-weight: bold; margin-bottom: 4px; margin-top: 0; text-transform: uppercase;">Servicio Nacional de Sanidad inocuidad y Calidad Agroalimentaria</h2>
+                  <h3 style="font-size: 18px; font-weight: bold; margin-top: 0; text-transform: uppercase;">Dirección General de inspección Fitozoosanitaria</h3>
+              </td>
+          </tr>
+
+      </table>'], 
+          'SetFooter'=>['
+          <table cellpadding="0" cellspacing="0" style="border: 1px solid black; border-top: none; border-bottom-color: black; width: 100%;">
+          <tr>
+              <td style="padding: 12px 8px;">
+                  <strong style="display: block;">Cadena original. Información del documento oficial que presenta que declara:</strong>
+                  <span style="display: block; font-size: 11px;">|LODH660920CZ6|ACTA DE RETENCIÓN|AR012018190556|AICM Sala E Internacional DF, Ciudad de México|22/05/2018|ANIMAL|Destruccion|HECTOR LOPEZ DAVALOS|MULTIPLE|APS012018365434|VARIOS DESCONOCIDO DESCONOCIDO|DESCONOCIDO|CABEZAS|LODH660920CZ6|HECTOR LOPEZ DAVALOS|2018-05-22|</span>
+              </td>
+          </tr>
+          <tr>
+              <td style="padding: 12px 8px;">
+                  <strong style="display: block;">Sello digital del autorizador del documento oficial:</strong>
+                  <span style="display: block; font-size: 11px;">LMzlEfeqflT5ZqOHjDaxQehOKcqai1/IjBvFtX1oYAahW6MfYx+PKwsPvda+y3R6UimLMZ0pREDcZBB0w+Yg0nVw8GCBzzuQ9nVCYO2W16Cb8yTlvMpbOSXYxqoKxd2mLOW5DLp++J6u/r0d2oYH7d/tOeVfRoJnAWgnymvNq9YdY8uD1BQPmuKqOWfbJwJ Qb72gIuxDZk+U6HfsMpaPfSt+Hayv1IW7zA56sVi/sV2QXO6BJh8t9f08qGmHRWJif8WGaxy/oqVoUR6z4BxawYmyaLSRF2V7BKuDkhEDJpnNWpFQGwnxidPT0xvrLXbrko7TuYAr2MF9tQA0UwuJ1w==</span>
+              </td>
+          </tr>
+      </table>{PAGENO}'],
+        ]
+    ]);
+    
+    // return the pdf output as per the destination setting
+    return $pdf->render();
     }
 
     
---
+++ views/site/_acta.php
@@ -0,0 +1,211 @@
+
+<body>
+
+    <table cellpadding="0" cellspacing="0" style="background-color: white; border: none; width: 100%;">
+        <tbody>
+                  
+
+            <tr>
+
+                <td>
+                    
+                    <table cellpadding="0" cellspacing="0" style="border-left: 1px solid black; border-right: 1px solid black; border-bottom: none; border-collapse: collapse; width: 100%;">
+                        <tr>
+                            <td colspan="3" style="border: none; padding: 2px 8px;"></td>
+                            <td style="border: none; padding: 2px 8px 2px 0;">
+                                <strong style="font-size: 16px;">FOLIO: </strong>
+                                <span style="font-size: 15px; text-decoration: underline; text-transform: uppercase;"> 23g4234ghj23</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="center" colspan="4" style="border: 1px solid black; border-top: none; padding: 2px 8px;">
+                                <h4 style="font-size: 18px; font-weight: bold; margin-top: 0; padding: 8px 0;">ACTA CIRCUNSTANCIADA</h4>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="2" style="border: 1px solid black; border-left: 1px solid black; border-right: 1px solid black; padding: 2px 8px;">
+                                <strong style="display: block;">OISA en: </strong>
+                                <span style="display: block;"> AICM Sala E Internacional DF. Ciudad de México</span>
+                            </td>
+                            <td style="border: 1px solid black; border-left: 1px solid black; border-right: 1px solid black; padding: 2px 8px;">
+                                <strong style="display: block;">Fecha: </strong>
+                                <span style="display: block;"> 22/05/2018</span>
+                            </td>
+                            <td style="border: 1px solid black; border-left: 1px solid black; border-right: 1px solid black; padding: 2px 8px;">
+                                <strong style="display: block;">Hora: </strong>
+                                <span style="display: block;"> 0:56 PM</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="3" style="border: 1px solid black; border-left: 1px solid black; border-right: 1px solid black; padding: 2px 8px;">
+                                <strong style="display: block;">Nombre del representante o particular: </strong>
+                                <span style="display: block;"> VARIOS DESCONOCIDO DESCONOCIDO</span>
+                            </td>
+                            <td style="border: 1px solid black; border-left: 1px solid black; border-right: 1px solid black; padding: 2px 8px;">
+                                <strong style="display: block;">Identificación: </strong>
+                                <span style="display: block;"> PASARPORTE: G00000001</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="4" style="border: 1px solid black; border-left: 1px solid black; border-right: 1px solid black; padding: 2px 8px;">
+                                <strong style="display: block;">Información complementaría: (Domicilio, CP.) </strong>
+                                <span style="display: block;"> VARIOS</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="3" style="border: 1px solid black; border-left: 1px solid black; border-right: 1px solid black; padding: 2px 8px;">
+                                <strong style="display: block;">Nombre del oficial: </strong>
+                                <span style="display: block;"> HECTOR LOPEZ DAVALOS</span>
+                            </td>
+                            <td style="border: 1px solid black; border-left: 1px solid black; border-right: 1px solid black; padding: 2px 8px;">
+                                <strong style="display: block;">No. de credencial: </strong>
+                                <span style="display: block;"> 331/14</span>
+                            </td>
+                        </tr>
+                    </table>
+
+                </td>
+
+            </tr>
+
+            <tr>
+
+                <td>
+
+                    <table cellpadding="0" cellspacing="0" style="width: 100%;">
+
+                        <tr>
+                            <td colspan="2" style="border-bottom: none; border-top: none; border-left: 1px solid black; border-right: 1px solid black; padding: 16px 12px 4px;">
+                                <h4 style="font-size: 18px; font-weight: bold; margin: 0;">ACTA</h4>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td colspan="2" style="border-bottom: none; border-top-color: transparent; border-left: 1px solid black; border-right: 1px solid black; padding: 4px 12px 24px;">
+
+                                <table cellpadding="0" cellspacing="0" style="border: 1px solid black; width: 100%;">
+                                    <thead>
+                                        <tr>
+                                            <th style="border: 1px solid black; padding: 12px 8px; text-align: center;">Folio:</th>
+                                            <th style="border: 1px solid black; padding: 12px 8px; text-align: center;">País de origen:</th>
+                                            <th style="border: 1px solid black; padding: 12px 8px; text-align: center;">País de procedencia:</th>
+                                            <th style="border: 1px solid black; padding: 12px 8px; text-align: center;">Mercancía:</th>
+                                            <th style="border: 1px solid black; padding: 12px 8px; text-align: center;">Cantidad:</th>
+                                            <th style="border: 1px solid black; padding: 12px 8px; text-align: center;">Unidad de medida:</th>
+                                            <th style="border: 1px solid black; padding: 12px 8px; text-align: center;">Detectado por:</th>
+                                            <th style="border: 1px solid black; padding: 12px 8px; text-align: center;">Dictaminadi por:</th>
+                                            <th style="border: 1px solid black; padding: 12px 8px; text-align: center;">Clave TEA:</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td style="border: 1px solid black; padding: 12px 8px; text-align: center;">APS343432424324</td>
+                                            <td style="border: 1px solid black; padding: 12px 8px; text-align: center;">Cualquiera</td>
+                                            <td style="border: 1px solid black; padding: 12px 8px; text-align: center;">Cualquiera</td>
+                                            <td style="border: 1px solid black; padding: 12px 8px; text-align: center;">DESCONOCIDO</td>
+                                            <td style="border: 1px solid black; padding: 12px 8px; text-align: center;">1.0</td>
+                                            <td style="border: 1px solid black; padding: 12px 8px; text-align: center;">CABEZAS</td>
+                                            <td style="border: 1px solid black; padding: 12px 8px; text-align: center;">OBSERVACION DIRECTA</td>
+                                            <td style="border: 1px solid black; padding: 12px 8px; text-align: center;">HECTOR LOPEZ DAVALOS</td>
+                                            <td style="border: 1px solid black; padding: 12px 8px; text-align: center;">N/A</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td colspan="2" style="border-bottom: none; border-top-color: transparent; border-left: 1px solid black; border-right: 1px solid black; padding: 4px 12px 24px;">
+                                <strong>Para su: </strong>
+                                <span>Destrucción</span>
+                            </td>
+                        </tr>
+                        
+                        <tr>
+                            <td style="border-bottom: none; border-top-color: transparent; border-left: 1px solid black; border-right: none; padding: 12px 2px 12px 12px; width: 180px;">
+                                <strong>En uso de la palabra del C: </strong>
+                            </td>
+                            <td style="border-bottom: 2px solid black; border-top-color: transparent; border-left: none; border-right: 1px solid black;">
+                                <span></span>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td colspan="2" style="border-bottom: none; border-top-color: transparent; border-left: 1px solid black; border-right: 1px solid black; padding: 12px 2px 12px 12px;"></td>
+                        </tr>
+
+                    </table>         
+
+                </td>
+
+            </tr>
+
+            <tr>
+
+                <td>
+
+                    <table cellpadding="0" cellspacing="0" style="border: 1px solid black; border-top-color: black; border-bottom-color: black; width: 100%;">
+                        <tr>
+                            <td style="border-bottom: 1px solid black; padding: 4px 8px;">
+                                <strong>MOTIVO: </strong>
+                                <span>PRUEBA</span>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td style="border-bottom: 1px solid black; padding: 12px 8px;">
+                                <p style="font-size: 14px; letter-spacing: 1px; margin: 0;">
+                                    <strong>Fundamento legal: </strong>
+                                    Con fundamento en los artículos 1, 6 y 7 fracciones XIII, XVIII, XIX, XXI, XXVIII y XLI, 15, 19 Fracciones I, incisos e) l) y IV, 23, 24, 26, 27 A, 29, 30 
+                                    <strong>fracción I</strong>, 46, 54 fracción III y 55 de la Ley Federal de Sanidad Vegetal; 1, 5, 6 fracciones I, II, III, IV, XI, XII, XIV, XX, XXII, LVII, LXIII y LXXI, 16 fracciones II, IV, VI, VII, VIII y XXI, 18, 24, 25, 26, 27, 32, 33, 35 fracciones I y IV, 36, 38, 39, 45 
+                                    <strong>fracción I</strong>, 46, <strong>50,</strong> 78, 126 fracción III, 127 fracciones I, V y VII, 128, 129, 130, 133, 136 fracción I y 138 fracción I de la Ley Federal de Sanidad Animal;1, 2 fracciones X, XIII y XIV, 7, 8 fracciones XXII, XXXVIII, XXXIX y XL, 10 fracciones I y II, 21, 95, 96, 103, 104, 105 fracción I, 107, 109, 111, 113, 114 y 124 de la Ley General de Pesca y Acuacultura Sustentables; Norma Oficial Mexicana No. <span style="letter-spacing: 0;">____________</span>; 1, 2, 3, 8 y 9 de la Ley Federal de Procedimiento Administrativo; 1, 54, 55, 58, 59, 60, 64, 65, 78 
+                                    <strong>fracción II, 79, 80,</strong> 131, 163 y 172 del Reglamento de la Ley Federal de Sanidad Vegetal; 1, 4, 35, 36, 37, 38, 41, 44, 47, <strong>48,</strong> 49, 50, 52, 59, 60, 61, 62, 67, 70, 72 y 135 del Reglamento de la Ley Federal de Sanidad Animal; 1, 2 inciso D, 16, 17 fracciones I, III, IV y XVIII, 44 y 46 del Reglamento Interior de la Secretaría de Agricultura, Ganadería, Desarrollo Rural, Pesca y Alimentación publicado en el DOF el día 25 de abril de 2012; 4, 14 fracciones XVIII, XXIII y XXIV y 17 del Reglamento Interior del Servicio Nacional de Sanidad, Inocuidad y Calidad Agroalimentaria y; en el ACUERDO que establece la clasificación y codificación de mercancías cuya importación está sujeta a regulación por parte de la Secretaría de Agricultura, Ganadería, Desarrollo Rural, Pesca y Alimentación, a través del Servicio Nacional de Sanidad, Inocuidad y Calidad Agroalimentaria, publicado en el DOF el día 3 de septiembre de 2012.
+                                </p>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td style="border-bottom: 1px solid black; padding: 4px 8px;">
+                                <strong>DESCRIPCIÓN DE HECHOS: </strong>
+                                <span>PRUEBA</span>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td style="padding: 4px 8px;">
+                                <strong>OBSERVACIONES: </strong>
+                                <span>PRUEBA</span>
+                            </td>
+                        </tr>
+
+                    </table>
+
+                </td>
+
+            </tr>
+
+            <tr>
+                <td>
+                    <table cellpadding="0" cellspacing="0" style="border: 1px solid black; border-top: none; border-bottom: none; width: 100%;">
+                        <tr>
+                            <td style="height: 300px;"></td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+
+            <tr>
+
+                <td>
+
+                  
+
+                </td>
+
+            </tr>
+
+        </tbody>
+    </table>
+    
+</body>
